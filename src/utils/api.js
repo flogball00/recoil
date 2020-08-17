37f8d2d1699e9de1ed755212e6b028c6d4544392
flogball00 <mleong28@gmail.com>
2020-08-16 21:53:47 -0700

    token

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,6 @@ import { GITHUB_API_URL } from "../constants";
 function client(url, endpoint, { body, ...customConfig } = {}) {
   const headers = {
     "Content-Type": "application/json",
-    Authorization: "token 77a73e87c1d9fd071669f5f2f668eec80c31c5ce",
   };
   const config = {
     method: body ? "POST" : "GET",
